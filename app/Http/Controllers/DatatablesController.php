a03512163d81b50a7c144c02bbc97eda3a413b1c
rexcool220 <rexcool220@gmail.com>
2017-08-11 11:48:25 +0800

    completed edit button for each row

--- app/Http/Controllers/DatatablesController.php
+++ app/Http/Controllers/DatatablesController.php
@@ -23,10 +23,29 @@ class DatatablesController extends Controller
     public function shippingRecordData()
     {
         $shippingRecord = ShippingRecord::select(['FB帳號', '品項', '單價', '數量', '匯款日期', '出貨日期', 'SerialNumber', '匯款編號', '確認收款', 'FBID', '備註', '月份', 'Active', '規格', 'ItemID']);
+
         return Datatables::of($shippingRecord)
             ->addIndexColumn()
             ->addColumn('edit', function ($shippingRecord) {
-                return '<button class="btn-edit btn-xs btn-primary" data-remote="'. $shippingRecord->FB帳號 .'"><span class="glyphicon glyphicon-edit"></span>&nbsp;</button>';
+                $shippingRecordArray = array(
+                    'FB帳號' => $shippingRecord->FB帳號,
+                    '品項' => $shippingRecord->品項,
+                    '單價' => $shippingRecord->單價,
+                    '數量' => $shippingRecord->數量,
+                    '匯款日期' => $shippingRecord->匯款日期,
+                    '出貨日期' => $shippingRecord->出貨日期,
+                    'SerialNumber' => $shippingRecord->SerialNumber,
+                    '匯款編號' => $shippingRecord->匯款編號,
+                    '確認收款' => $shippingRecord->確認收款,
+                    'FBID' => $shippingRecord->FBID,
+                    '備註' => $shippingRecord->備註,
+                    '月份' => $shippingRecord->月份,
+                    'Active' => $shippingRecord->Active,
+                    '規格' => $shippingRecord->規格,
+                    'ItemID' => $shippingRecord->ItemID
+                );
+
+                return '<button class="btn-edit btn-xs btn-primary" data-remote=\''. json_encode($shippingRecordArray) .'\'><span class="glyphicon glyphicon-edit"></span>&nbsp;</button>';
             })
             ->addColumn('delete', function ($shippingRecord) {
                 return '<button class="btn-delete btn-xs btn-primary" data-remote="/shippingRecord/'. $shippingRecord->SerialNumber .'"><span class="glyphicon glyphicon-trash"></span>&nbsp;</button>';
