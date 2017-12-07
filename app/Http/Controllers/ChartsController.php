1b9d0dd83833d19ae45ccc2df1073a98f42704f5
rexcool220 <rexcool220@gmail.com>
2017-12-07 23:54:03 +0800

    show 2018 data for charts

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -17,7 +17,7 @@ class ChartsController extends Controller
         Request::get('token');
 
 
-        $revenues = DB::select('SELECT SUM(ShippingRecord.單價 * ShippingRecord.數量) AS revenue, SUM((ShippingRecord.單價 - ItemCategory.成本) * ShippingRecord.數量) AS profit, ShippingRecord.月份 AS month FROM ShippingRecord,ItemCategory WHERE(ShippingRecord.ItemID = ItemCategory.ItemID AND ShippingRecord.規格 = ItemCategory.規格) AND ShippingRecord.月份 REGEXP \'^2017[0-1][0-9]\' AND ShippingRecord.月份 > 201704 GROUP BY ShippingRecord.月份');
+        $revenues = DB::select('SELECT SUM(ShippingRecord.單價 * ShippingRecord.數量) AS revenue, SUM((ShippingRecord.單價 - ItemCategory.成本) * ShippingRecord.數量) AS profit, ShippingRecord.月份 AS month FROM ShippingRecord,ItemCategory WHERE(ShippingRecord.ItemID = ItemCategory.ItemID AND ShippingRecord.規格 = ItemCategory.規格) AND ShippingRecord.月份 REGEXP \'^201[7-8][0-1][0-9]\' AND ShippingRecord.月份 > 201704 GROUP BY ShippingRecord.月份');
 
         return view('admin.dashboard',compact('revenues'));
     }
